7e4980bc3c5dba49d1e7a912de4afec000caf127
go140point6 <ryan@go140point6.com>
2022-06-01 18:45:14 -0700

    rewrite for discord.js v13

--- README.md
+++ README.md
@@ -1,25 +1,35 @@
 # Cryptocurrency Price Status Discord Bot
 
-This bot will update its status message with the current price of the desired cryptocurrency.
+This bot will update its status message:
+1. With the current price
+2. Changing colors to denote an increase or decrease from last check, if any
+3. Changing arrow direction to denote and increase or decrease from last check, if any
 
-Is based on [Minecraft Player Count Discord Bot](https://github.com/SpencerTorres/Minecraft-Player-Count-Discord-Bot) by Spencer Torres
+It is based on [Cryptocurrency-Status-Discord-Bot](https://github.com/cferreras-zz/Cryptocurrency-Status-Discord-Bot) by Carlos Ferreras
+
+who based it on [Minecraft Player Count Discord Bot](https://github.com/SpencerTorres/Minecraft-Player-Count-Discord-Bot) by Spencer Torres
+
+It takes inspiration for how it works and looks from [discord-stock-ticker](https://github.com/rssnyder/discord-stock-ticker) by Riley Snyder
 
 ![Example bot setup.](https://i.imgur.com/aDy2dpj.png)
 
 ## How to use
 
-This bot is really easy to use.
+This bot is really easy to use
 
 Just follow these steps:
 1. Have [Node.JS](https://nodejs.org) installed.
 2. Clone this repository to a folder on your computer.
 3. Open a terminal in that folder, and install the packages with `npm install`
-4. Open the `.env-template` file and configure it to your liking.
+4. Open the `.env-template` file and configure it to your liking, saving as `.env`
 
 For information on getting a bot token, follow the steps on [the Discord developer documentation.](https://discordapp.com/developers/docs/intro)
 
+Note: this code has been updated to work with discord.js v13 and you will have to give you bot the priviledged gateway intents (Presence and Server Members)
+
 ## Extra Info
 
-This was created for my Discord server, but I wanted to share it with added flexibility for anyone to use.
+This relies on the free API hosted at https://api.coingecko.com
 
-This relies on the API hosted at https://api.coingecko.com
+I am not a Javascript developer (I'm not a real developer at all actually, I just bang on my keyboard until something mostly works).  This code is very
+crude and inelegant but worked for me.  I'm sure it can be made much more efficient and I might circle back to it at some point.
--- index.js
+++ index.js
@@ -1,44 +1,221 @@
 require('dotenv').config() // Load .env file
 const axios = require('axios')
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const { Client, Intents } = require('discord.js')
+
+const myIntents = new Intents();
+myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS)
+
+// Create a new client instance
+const client = new Client({ intents: myIntents })
+
+const up = "\u2B08"
+const down = "\u2B0A"
+const mid = "\u22EF"
+
+var lastPrice
+var currentPrice
+var arrow
+
+async function clearRoles () {
+  const guild = client.guilds.cache.get(`${process.env.SERVER_ID}`)
+  if(!guild) {
+    try {
+      const guild = await client.guilds.fetch(`${process.env.SERVER_ID}`)
+    } catch (error) {
+      return console.log(`Error while fetching the guild: `, error)
+    }
+  }
+
+  const red = guild.roles.cache.find(role => role.name === 'tickers-red')
+  if(!red) {
+    try {
+      const red = await guild.roles.cache.find(role => role.name === 'tickers-red')
+    } catch (error) {
+      return console.log(`Error while fetching the role: `, error)
+    }
+  }
+
+  const green = guild.roles.cache.find(role => role.name === 'tickers-green')
+  if(!green) {
+    try {
+      const green = await guild.roles.cache.find(role => role.name === 'tickers-green')
+    } catch (error) {
+      return console.log(`Error while fetching the role: `, error)
+    }
+  }
+
+  const member = guild.members.cache.get(`${process.env.BOT_ID}`)
+  if(!member) {
+    try {
+      member = await guild.members.fetch(`${process.env.BOT_ID}`)
+    } catch (error) {
+      return console.log(`Error while fetching the member: `, error)
+    }
+  }
+
+  member.roles.remove(red) && member.roles.remove(green)
+}
+
+async function setRed () {
+  const guild = client.guilds.cache.get(`${process.env.SERVER_ID}`)
+  if(!guild) {
+    try {
+      const guild = await client.guilds.fetch(`${process.env.SERVER_ID}`)
+    } catch (error) {
+      return console.log(`Error while fetching the guild: `, error)
+    }
+  }
+
+  const red = guild.roles.cache.find(role => role.name === 'tickers-red')
+  if(!red) {
+    try {
+      const red = await guild.roles.cache.find(role => role.name === 'tickers-red')
+    } catch (error) {
+      return console.log(`Error while fetching the role: `, error)
+    }
+  }
+
+  const green = guild.roles.cache.find(role => role.name === 'tickers-green')
+  if(!green) {
+    try {
+      const green = await guild.roles.cache.find(role => role.name === 'tickers-green')
+    } catch (error) {
+      return console.log(`Error while fetching the role: `, error)
+    }
+  }
+
+  const member = guild.members.cache.get(`${process.env.BOT_ID}`)
+  if(!member) {
+    try {
+      member = await guild.members.fetch(`${process.env.BOT_ID}`)
+    } catch (error) {
+      return console.log(`Error while fetching the member: `, error)
+    }
+  }
+
+  if(red) {
+    member.roles.add(red) && member.roles.remove(green)
+  }
+}
+
+async function setGreen () {
+  const guild = client.guilds.cache.get(`${process.env.SERVER_ID}`)
+  if(!guild) {
+    try {
+      const guild = await client.guilds.fetch(`${process.env.SERVER_ID}`)
+    } catch (error) {
+      return console.log(`Error while fetching the guild: `, error)
+    }
+  }
+
+  const red = guild.roles.cache.find(role => role.name === 'tickers-red')
+  if(!red) {
+    try {
+      const red = await guild.roles.cache.find(role => role.name === 'tickers-red')
+    } catch (error) {
+      return console.log(`Error while fetching the role: `, error)
+    }
+  }
+
+  const green = guild.roles.cache.find(role => role.name === 'tickers-green')
+  if(!green) {
+    try {
+      const green = await guild.roles.cache.find(role => role.name === 'tickers-green')
+    } catch (error) {
+      return console.log(`Error while fetching the role: `, error)
+    }
+  }
+
+  const member = guild.members.cache.get(`${process.env.BOT_ID}`)
+  if(!member) {
+    try {
+      member = await guild.members.fetch(`${process.env.BOT_ID}`)
+    } catch (error) {
+      return console.log(`Error while fetching the member: `, error)
+    }
+  }
+
+  if(green) {
+    member.roles.add(green) && member.roles.remove(red)
+  }
+}
+
+function getInitialPrice() {
+  //API for price data
+  axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${process.env.PREFERRED_CURRENCY}&ids=${process.env.COIN_ID}`).then(res => {
+    // If we got a valid response
+    if(res.data && res.data[0].current_price && res.data[0].price_change_percentage_24h) {
+      clearRoles()
+      lastPrice = res.data[0].current_price.toFixed(4) || 0 // Default to zero
+      let priceChange = res.data[0].price_change_percentage_24h || 0 // Default to zero
+      let symbol = res.data[0].symbol || '?'
+      client.user.setPresence({
+        activities: [{
+          name: `24hr: ${priceChange.toFixed(2)}%`,
+          type: `WATCHING`
+          }]
+        })
+
+        arrow = mid
+        client.guilds.cache.find(guild => guild.id === process.env.SERVER_ID).me.setNickname(`${symbol.toUpperCase()} ${arrow} ${process.env.CURRENCY_SYMBOL}${lastPrice}`)
+
+    //console.log('Initial price to', lastPrice)
+    //console.log('priceChange 24h is', priceChange)
+    //console.log('symbol is', symbol)
+    }
+    else
+      console.log('Could not load player count data for', process.env.COIN_ID)
+
+  }).catch(err => console.log('Error at api.coingecko.com data:', err))
+}
 
 function getPrices() {
+  //API for price data
+  axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${process.env.PREFERRED_CURRENCY}&ids=${process.env.COIN_ID}`).then(res => {
+    // If we got a valid response
+    if(res.data && res.data[0].current_price && res.data[0].price_change_percentage_24h) {
+      currentPrice = res.data[0].current_price.toFixed(4) || 0 // Default to zero
+      let priceChange = res.data[0].price_change_percentage_24h || 0 // Default to zero
+      let symbol = res.data[0].symbol || '?'
+      client.user.setPresence({
+        activities: [{
+          name: `24hr: ${priceChange.toFixed(2)}%`,
+          type: `WATCHING`
+          }]
+        })
 
+      if (currentPrice > lastPrice) {
+        //console.log('up')
+        arrow = up
+        setGreen()
+        } else if (currentPrice < lastPrice) {
+          //console.log('down')
+          arrow = down
+          setRed()
+        } else {
+          //console.log('same')
+        }
 
-	// API for price data.
-	axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${process.env.PREFERRED_CURRENCY}&ids=${process.env.COIN_ID}`).then(res => {
-		// If we got a valid response
-		if(res.data && res.data[0].current_price && res.data[0].price_change_percentage_24h) {
-			let currentPrice = res.data[0].current_price || 0 // Default to zero
-			let priceChange = res.data[0].price_change_percentage_24h || 0 // Default to zero
-			let symbol = res.data[0].symbol || '?' 
-			client.user.setPresence({
-				game: {
-					// Example: "Watching -5,52% | BTC"
-					name: `${priceChange.toFixed(2)}% | ${symbol.toUpperCase()}`,
-					type: 3 // Use activity type 3 which is "Watching"
-				}
-			})
+        client.guilds.cache.find(guild => guild.id === process.env.SERVER_ID).me.setNickname(`${symbol.toUpperCase()} ${arrow} ${process.env.CURRENCY_SYMBOL}${currentPrice}`)
+        //console.log('Current price to', lastPrice)
+        //console.log('priceChange 24h is', priceChange)
 
-			client.guilds.find(guild => guild.id === process.env.SERVER_ID).me.setNickname(`${(currentPrice).toLocaleString().replace(/,/g,process.env.THOUSAND_SEPARATOR)}${process.env.CURRENCY_SYMBOL}`)
+        lastPrice = currentPrice
 
-			console.log('Updated price to', currentPrice)
-		}
-		else
-			console.log('Could not load player count data for', process.env.COIN_ID)
+    }
+    else
+      console.log('Could not load player count data for', process.env.COIN_ID)
 
-	}).catch(err => console.log('Error at api.coingecko.com data:', err))
+  }).catch(err => console.log('Error at api.coingecko.com data:', err))
 }
 
 // Runs when client connects to Discord.
 client.on('ready', () => {
-	console.log('Logged in as', client.user.tag)
-
-	getPrices() // Ping server once on startup
-	// Ping the server and set the new status message every x minutes. (Minimum of 1 minute)
-	setInterval(getPrices, Math.max(1, process.env.MC_PING_FREQUENCY || 1) * 60 * 1000)
+  console.log('Logged in as', client.user.tag)
+  getInitialPrice() // Ping server once on startup
+  // Ping the server and set the new status message every x minutes. (Minimum of 1 minute)
+  setInterval(getPrices, Math.max(1, process.env.MC_PING_FREQUENCY || 1) * 60 * 1000)
 })
 
-// Login to Discord
-client.login(process.env.DISCORD_TOKEN)
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
